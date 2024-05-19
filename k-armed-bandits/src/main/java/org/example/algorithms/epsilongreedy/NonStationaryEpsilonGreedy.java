4c162e575943d05eb5d22e5b847502352cf04db4
sadelcarpio <sadelcarpioa@gmail.com>
2024-05-19 13:40:30 -0500

    optimize imports

--- k-armed-bandits/src/main/java/org/example/algorithms/epsilongreedy/NonStationaryEpsilonGreedy.java
+++ k-armed-bandits/src/main/java/org/example/algorithms/epsilongreedy/NonStationaryEpsilonGreedy.java
@@ -1,13 +1,7 @@
 package org.example.algorithms.epsilongreedy;
 
-import org.example.utils.ArrayUtils;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class NonStationaryEpsilonGreedy extends EpsilonGreedy{
     private final double stepSize;
-    private double[] qNonStat;
 
     public NonStationaryEpsilonGreedy(double epsilon, double stepSize) {
         super(epsilon);
