ae3351e40323cbb09e017327bc8f90fc8da10395
sadelcarpio <sadelcarpioa@gmail.com>
2024-05-23 17:14:06 -0500

    Tests for ArrayUtils

--- k-armed-bandits/src/test/java/TestArrayUtils.java
+++ k-armed-bandits/src/test/java/TestArrayUtils.java
@@ -14,7 +14,20 @@ public class TestArrayUtils {
         double[] testList2 = new double[]{1., 3., 1., 3., 4., 2., 4.};
         List<Integer> argmax = ArrayUtils.getArgmax(testList);
         List<Integer> argmax2 = ArrayUtils.getArgmax(testList2);
-        assertEquals(argmax, List.of(3));
-        assertEquals(argmax2, Arrays.asList(4, 6));
+        assertEquals(List.of(3), argmax);
+        assertEquals(Arrays.asList(4, 6), argmax2);
+    }
+
+    @Test
+    public void testTrueRatio() {
+        boolean[][] testArrays = new boolean[][] {{true, true, false},
+                {false, false, true, false},
+                {false, false, false, false},
+                {true, true, true, true, true}};
+        double[] expectedRatios = new double[]{2./3, 0.25, 0., 1.};
+        for (int i = 0; i < 4; i++) {
+            double trueRatio = ArrayUtils.trueRatio(testArrays[i]);
+            assertEquals(expectedRatios[i], trueRatio, 0.001);
+        }
     }
 }
